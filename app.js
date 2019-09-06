d0d822032954bbef9b92fe6126152597cee0df2d
Zablon18 <zablah.developer@gmail.com>
2019-09-05 22:43:39 -0500

    Added POI to Emergency message

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const accountSid = 'AC85eb3c7abf9a7aeaac0dcfbd5c70efa8';
 const authToken = 'ef83b8fb6bb6ee176d437275c8bbaf5e';
 const client = require('twilio')(accountSid, authToken);
+const axios = require('axios');
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,12 +16,23 @@ app.get('/', function (req, res) {
 });
 
 app.post('/sos', async (req, res) => {
+    
+    const response = await axios({
+        url: `https://api.foursquare.com/v2/venues/search?ll=${req.body.lat},${req.body.lon}&client_id=HDJQ0G4WXC3LHTIDNKN5VSSCIRUWWFN3EH4CLSD0CMC3CNEM&client_secret=5DFNH2VNB2Z3UWLLBYVUL2PC1SBBWQENCEFJGOSH3XDOEB2I&v=20190905`,
+        method: 'get'
+    });
+    
+    const nombrePOI = response.data.response.venues[0].name;
+    let direccion = response.data.response.venues[0].location.formattedAddress;
+    direccion = direccion[0] + ' '+ direccion [1] + ' '+ direccion[2];
+    console.log(nombrePOI, direccion);
+    
     const sms = new Promise((resolve,reject) => {
         client.messages
         .create({
             from: '+12563630857',
             to: '+528711499240',
-            body: `SOS Necesito ayuda en: ${req.body.lat} : ${req.body.lon}`
+            body: `SOS Necesito ayuda en: https://maps.google.com/?q=${req.body.lat},${req.body.lon} cerca de ${nombrePOI} con direccion en ${direccion}`
         })
         .then(message => {
             resolve();
@@ -32,7 +44,7 @@ app.post('/sos', async (req, res) => {
             .create({
                 from: 'whatsapp:+14155238886',
                 to: 'whatsapp:+5218711499240',
-                body: `SOS Necesito ayuda en: ${req.body.lat} : ${req.body.lon}`
+                body: `SOS Necesito ayuda en: https://maps.google.com/?q=${req.body.lat},${req.body.lon}`
             })
             .then(message => {
                 resolve();
