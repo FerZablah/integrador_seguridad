386d0b64b53a76109bdeccc04e347de255fb8311
Zablon18 <zablah.developer@gmail.com>
2019-10-15 19:38:42 -0500

    Added insertEvento procedure

---
+++ MySQL/Procedures/insertEvento.sql
@@ -0,0 +1,8 @@
+DROP PROCEDURE IF EXISTS insertEvento;
+DELIMITER //
+CREATE PROCEDURE insertEvento(IN input_fecha_hora DATETIME, IN input_liga VARCHAR(255), IN input_uid_usuario VARCHAR(36))
+BEGIN
+INSERT INTO Evento(fecha_hora, liga, uid_usuario) values(input_fecha_hora, input_liga, input_uid_usuario);
+SELECT last_insert_id() as idEvento;
+END //
+DELIMITER ;
