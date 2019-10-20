5b2a31bf07b0695c57d06f11903174563bb6d8d2
Zablon18 <zablah.developer@gmail.com>
2019-10-20 10:35:34 -0500

    Cors setup applied only to dev environment

--- Express/app.js
+++ Express/app.js
@@ -1,30 +1,35 @@
 //Importar paquetes de NPM
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+} 
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const port = process.env.PORT || 4000; //Puerto 4000 o definido por Heroku
 const bodyParser = require("body-parser");
 const db = require('tnc_mysql_connector');
-const cors = require("cors");
 
 //Ajustes de servidor con express
 app.use(bodyParser.json());//Permite recibir cuerpos JSON
 app.use(bodyParser.urlencoded({ extended: true }));
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+if (process.env.NODE_ENV !== 'production'){
+  const cors = require("cors");
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+  app.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
     }
-    return callback(null, true);
-  }
-}));
+  }));
+} 
+
 //Esperamos a tener conexion a base de datos para encender servidor
 db.connect().then( async () => {
     require('./startup/routes.js')(app);
